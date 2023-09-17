74ee4232cb5cd6aff043d19e558d0f54dd583aaa
Ane França <127628489+anedfranca@users.noreply.github.com>
2023-09-17 16:14:31 -0300

    Update av_aula4.py

--- av_aula4.py
+++ av_aula4.py
@@ -1,5 +1,16 @@
-x=0
+'''Escreva um programa em python que leia números inteiros do teclado.
+O programa deve ler os números até que o usuário digite 0 (zero). No final da execução, 
+exiba a quantidade de números digitados, assim como a soma e a média aritmética.'''
 
-while x>=0:
-
-    print('Olá mundo!')
+soma = 0
+quantidade = 0
+while True:
+    n = int(input("Digite um número inteiro: "))
+    if n == 0:
+        break
+    soma = soma + n
+    quantidade = quantidade + 1
+    media = soma / quantidade
+print(f''' A quantidade de números digitados é {quantidade}
+      A soma dos números resultam em {soma}
+      A média aritmétrica é {media}''')
